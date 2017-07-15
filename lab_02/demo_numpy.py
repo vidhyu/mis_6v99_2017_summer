7fbdda10b933ac3ab1bfb9f7c6724ae7fde95355
vidhyu <vidhyullatha.aluri@gmail.com>
2017-07-14 19:14:31 -0500

    lab code final

--- lab_02/demo_numpy.py
+++ lab_02/demo_numpy.py
@@ -1,23 +1,19 @@
-# first Lab 
-csv_header = 'tail_number,origin,destination,departure_time,arrival_time'
-file_name = 'flight_schedule.csv'
-def print_flight_schedule(fn, csv_hdr, flt_sched):
-    with open(fn,'wt') as f:
-        print(csv_hdr, file=f)
-        for s in flt_sched:
-            print(','.join(s), file=f)
+# second Lab 
+
+import numpy as np
+import sys
+
+
+a = np.arange(15).reshape(3,5) # my array
+
+fn = 'demo_numpy.txt' # file name
+
+with open(fn,'w') as f:   # opening the file name as write
+    sys.stdout = f
+    print("array:", a)
+    print("array shape:", a.shape)
+    print("array size:", a.size)
+    print("array item size:", a.itemsize)
+    print("array number of dimensions", a.ndim)
+    print("array data type:", a.dtype)
             
-airports = [['AUS', 1, 25], ['DAL', 2, 30], ['HOU',3, 35]]
-
-flight_times = [['AUS', 'DAL', 50], ['DAL', 'AUS', 50], ['AUS','HOU', 45 ], ['HOU','AUS', 45 ], ['DAL','HOU', 65 ], ['HOU','DAL', 65 ]]
-
-# add the flights for T4, T5 and T6 to the list of lists below
-flight_schedule = [['T1','AUS','DAL','0600','0650'],
-                   ['T2','DAL','HOU','0600','0705'],
-                   ['T3','DAL','HOU','0600','0705'],
-                   ['T4','HOU','AUS','0600','0645'],
-                   ['T5','HOU','DAL','0600','0705'],
-                   ['T6','HOU','DAL','0600','0705']]
-
-
-print_flight_schedule(file_name, csv_header, flight_schedule)
