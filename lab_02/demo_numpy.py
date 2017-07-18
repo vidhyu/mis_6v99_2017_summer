42c0e195d67f5c1d532a65d311408ba2650d4a7b
vidhyu <vidhyullatha.aluri@gmail.com>
2017-07-18 09:45:48 -0500

    modifications to match the solution

--- lab_02/demo_numpy.py
+++ lab_02/demo_numpy.py
@@ -10,10 +10,10 @@ fn = 'demo_numpy.txt' # file name
 
 with open(fn,'w') as f:   # opening the file name as write
     sys.stdout = f
-    print("array:", a)
-    print("array shape:", a.shape)
-    print("array size:", a.size)
-    print("array item size:", a.itemsize)
-    print("array number of dimensions", a.ndim)
-    print("array data type:", a.dtype)
+    print(a)
+    print(a.shape)
+    print(a.size)
+    print(a.itemsize)
+    print(a.ndim)
+    print(a.dtype)
             
